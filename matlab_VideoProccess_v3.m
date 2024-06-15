f69838f5c147e8e7186ea916fa97bb91c210e378
Cindy Huang <145233557+sijiehuang34@users.noreply.github.com>
2024-06-15 17:15:41 -0400

    Add files via upload

--- matlab_VideoProccess_v3.m
+++ matlab_VideoProccess_v3.m
@@ -99,135 +99,157 @@ function [image,angle] = applyOpenCVOperations(image,angle,timestamp,speedVideo)
     % L = a label matrix of each shape
     [B,L] = bwboundaries(image,"noholes");
 
-    % Display each shape in a different color
-    % 0.5 sets the background gray
-    % @jet = a type of color map
-    imshow(label2rgb(L,@jet,[.5 .5 .5]))
-
-    hold on
-
-    % Make the boundaries black for visualization
-
-    for i = 1:length(B) % for i-many shapes
-      boundary = B{i}; % the boundary coordinates for the i-th shape
-      plot(boundary(:,2), boundary(:,1), "k", LineWidth = 3) % plot coord. in white
-    end
-
-    %% Obtain shape properties
-    stats = regionprops(L,"Extrema","Centroid","Area");
-
-    % Single out the punch, store & display properties
-    for i = 1:length(stats)
+    %% Check if there are 3 objects detected, if not, skip to the next image
+    if isequal(size(B), [3, 1])
+        %% Display each shape in a different color
+        % 0.5 sets the background gray
+        % @jet = a type of color map
+        imshow(label2rgb(L,@jet,[.5 .5 .5]))
     
-        % Area (pxl)
-        area = stats(i).Area;
-        if area ~= max([stats.Area]) && area ~= min([stats.Area]) 
-        % should work even when punch isn't visible
+        hold on
     
-            % Extremas
-            extrema = stats(i).Extrema; % get the extrema for the i-th region
-            plot(extrema(:,1), extrema(:,2), 'rx', 'LineWidth', 2, ...
-                "MarkerSize", 10); % plot the extrema points in red
+        % Make the boundaries black for visualization
     
-            % Outline the leftmost edge
-            x_Lbottom = stats(i).Extrema(7,1); % left-bottom x
-            x_Ltop = stats(i).Extrema(8,1); % left-top x
-            y_Lbottom = stats(i).Extrema(7,2); % left-bottom y
-            y_Ltop = stats(i).Extrema(8,2); % left-top y
-            line([x_Lbottom, x_Ltop], [y_Lbottom, y_Ltop], 'Color', 'r', ...
-                'LineWidth', 2);
-    
-            % Calculate the angle deviation
-                % Positive angle: tilted towards the right (anticlockwise)
-                % Negative angle: tilted towards the left (clockwise)
-            height = y_Lbottom - y_Ltop;
-            base = x_Lbottom - x_Ltop; 
-            angle_dev = round(rad2deg(atan(base / height)), 2);
-            angleString = "Angle (Punch): " + angle_dev + "°";
-            text(400, 50, angleString, 'FontSize', 14, 'Color', 'k', ...
-                'FontWeight', 'bold');
-    
-            % Centroid
-            centroid = stats(i).Centroid;
-            plot(centroid(1,1), centroid(1,2), 'r*', 'LineWidth', 2, ...
-                "MarkerSize", 10)
-            centroidString = sprintf('Centroid (Punch): (%.2f, %.2f)', ...
-                centroid(1,1), centroid(1,2));
-            text(400, 25, centroidString, 'FontSize', 14, 'Color', 'k', ...
-                'FontWeight', 'bold');
-    
+        for i = 1:length(B) % for i-many shapes
+          boundary = B{i}; % the boundary coordinates for the i-th shape
+          plot(boundary(:,2), boundary(:,1), "k", LineWidth = 3) % plot coord. in white
         end
-    end
 
-    %% Match & append the boundary data to the stats structure
-    [stats.Boundary] = deal([]);
-
-    for i = 1:length(stats)
-        area = stats(i).Area;
+        %% Obtain shape properties
+        stats = regionprops(L,"Extrema","Centroid","Area");
     
-        % BY THE SCANNING ORDER OF THE LABEL MATRIX: LEFT TO RIGHT
-    
-        % 1 = PART
-        if area == min([stats.Area])
-            stats(i).Boundary = B{1};
-        % 3 = DIE
-        elseif area == max([stats.Area])
-            stats(i).Boundary = B{3};
-        % 2 = PUNCH
-        else 
-            stats(i).Boundary = B{2};
-        end
+        % Single out the punch, store & display properties
+        for i = 1:length(stats)
         
-    end
-
-    %% Find midpoints of the part
-    part_midpoints = []; % initialize a matrix to store the midpoint coords
-    
-    totalCol = 720;
-    
-    for currentCol = 1:totalCol % loop through every column of the image
-        % Find if the current column is in the x-coordinates of the part
-        if any(B{1}(:,2) == currentCol)
-            % Find all corresponding y-coords for this x-coord (column)
-            index = find(B{1}(:,2) == currentCol);
-            y_coords = B{1}(index, 1);
-            % Define & store midpoints
-            midY = ( max(y_coords) + min(y_coords) ) / 2;
-            midPt = [currentCol, midY];
-            part_midpoints = [part_midpoints; midPt];
+            % Area (pxl)
+            area = stats(i).Area;
+            if area ~= max([stats.Area]) && area ~= min([stats.Area]) 
+            % should work even when punch isn't visible
+        
+                % Extremas
+                extrema = stats(i).Extrema; % get the extrema for the i-th region
+                plot(extrema(:,1), extrema(:,2), 'rx', 'LineWidth', 2, ...
+                    "MarkerSize", 10); % plot the extrema points in red
+        
+                % Outline the leftmost edge
+                x_Lbottom = stats(i).Extrema(7,1); % left-bottom x
+                x_Ltop = stats(i).Extrema(8,1); % left-top x
+                y_Lbottom = stats(i).Extrema(7,2); % left-bottom y
+                y_Ltop = stats(i).Extrema(8,2); % left-top y
+                line([x_Lbottom, x_Ltop], [y_Lbottom, y_Ltop], 'Color', 'r', ...
+                    'LineWidth', 2);
+        
+                % Calculate the angle deviation
+                    % Positive angle: tilted towards the right (anticlockwise)
+                    % Negative angle: tilted towards the left (clockwise)
+                height = y_Lbottom - y_Ltop;
+                base = x_Lbottom - x_Ltop; 
+                angle_dev = round(rad2deg(atan(base / height)), 2);
+                angleString = "Angle (Punch): " + angle_dev + "°";
+                text(400, 50, angleString, 'FontSize', 14, 'Color', 'k', ...
+                    'FontWeight', 'bold');
+        
+                % Centroid
+                centroid = stats(i).Centroid;
+                plot(centroid(1,1), centroid(1,2), 'r*', 'LineWidth', 2, ...
+                    "MarkerSize", 10)
+                centroidString = sprintf('Centroid (Punch): (%.2f, %.2f)', ...
+                    centroid(1,1), centroid(1,2));
+                text(400, 25, centroidString, 'FontSize', 14, 'Color', 'k', ...
+                    'FontWeight', 'bold');
+        
+            end
         end
-    end
-
-    %% Draw the midpoints
-    for i = 1:10:length(part_midpoints)
-        plot(part_midpoints(i,1), part_midpoints(i,2), 'r.', 'MarkerSize', 10)
-    end
     
-    hold off;
-
-    %% Display encoder data for the part
-    partString = "Angle (Part): " + angle + "°";
-    text(20, 400, partString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+        %% Match & append the boundary data to the stats structure
+        [stats.Boundary] = deal([]);
     
-    timeString = "Time: " + timestamp + " sec";
-    text(20, 500, timeString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+        for i = 1:length(stats)
+            area = stats(i).Area;
+        
+            % BY THE SCANNING ORDER OF THE LABEL MATRIX: LEFT TO RIGHT
+        
+            % 1 = PART
+            if area == min([stats.Area])
+                stats(i).Boundary = B{1};
+            % 3 = DIE
+            elseif area == max([stats.Area])
+                stats(i).Boundary = B{3};
+            % 2 = PUNCH
+            else 
+                stats(i).Boundary = B{2};
+            end
+            
+        end
+    
+        %% Find midpoints of the part
+        part_midpoints = []; % initialize a matrix to store the midpoint coords
+        
+        totalCol = 720;
+        
+        for currentCol = 1:totalCol % loop through every column of the image
+            % Find if the current column is in the x-coordinates of the part
+            if any(B{1}(:,2) == currentCol)
+                % Find all corresponding y-coords for this x-coord (column)
+                index = find(B{1}(:,2) == currentCol);
+                y_coords = B{1}(index, 1);
+                % Define & store midpoints
+                midY = ( max(y_coords) + min(y_coords) ) / 2;
+                midPt = [currentCol, midY];
+                part_midpoints = [part_midpoints; midPt];
+            end
+        end
+    
+        %% Draw the midpoints
+        for i = 1:10:length(part_midpoints)
+            plot(part_midpoints(i,1), part_midpoints(i,2), 'r.', 'MarkerSize', 10)
+        end
 
-    speedString = "Video Speed Up: " + speedVideo + "X";
-    text(20, 525, speedString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+        %% Display encoder data for the part
+        partString = "Angle (Part): " + angle + "°";
+        text(20, 25, partString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+        
+        timeString = "Time: " + timestamp + " sec";
+        text(20, 500, timeString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+    
+        speedString = "Video Speed Up: " + speedVideo + "X";
+        text(20, 525, speedString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+    
+        hold off;
+    
+        %% Let whatever is shown in the figure window become the new image
+        % Capture the figure as an image
+        frame = getframe(gcf); % gcf gets the current figure handle///fig gets the entire figure
+        % Convert the frame to an image matrix
+        imageMatrix = frame2im(frame);
+        % Store the image matrix in a variable
+        image = imageMatrix;
 
-    %% Let whatever is shown in the figure window become the new image
-    % Capture the figure as an image
-    frame = getframe(gcf); % gcf gets the current figure handle///fig gets the entire figure
-    % Convert the frame to an image matrix
-    imageMatrix = frame2im(frame);
-    % Store the image matrix in a variable
-    image = imageMatrix;
+    %% Not 3 objects dectected
+    else
+        %% Display encoder data for the part
+        partString = "Angle (Part): " + angle + "°";
+        text(20, 25, partString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+        
+        timeString = "Time: " + timestamp + " sec";
+        text(20, 500, timeString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+    
+        speedString = "Video Speed Up: " + speedVideo + "X";
+        text(20, 525, speedString, 'FontSize', 14, 'Color', 'k', 'FontWeight', 'bold');
+    
+        hold off;
+    
+        %% Let whatever is shown in the figure window become the new image
+        % Capture the figure as an image
+        frame = getframe(gcf); % gcf gets the current figure handle///fig gets the entire figure
+        % Convert the frame to an image matrix
+        imageMatrix = frame2im(frame);
+        % Store the image matrix in a variable
+        image = imageMatrix;
+
+    end
 
     %% cindyfx () ends;
-
-    % image = insertText(image, [20, 60], ['Angle (Part): ', num2str(angle), '°'], 'FontSize', 18, 'TextColor', 'white', 'BoxOpacity', 0);
-    % image = insertText(image, [20, 90], ['Time: ', num2str(timestamp), ' sec'], 'FontSize', 18, 'TextColor', 'white', 'BoxOpacity', 0);
-    % image = insertText(image, [20, 120], ['Video Speed Up ', num2str(speedVideo), 'X'], 'FontSize', 18, 'TextColor', 'white', 'BoxOpacity', 0);
 
 end
 
